beede872085f6e5d65f4b923b9eb7f8165f639e2
ChaseRensberger <chasehrensberger@gmail.com>
2024-04-25 11:23:29 -0400

    can send files to s3

--- image/src/test.mjs
+++ image/src/test.mjs
@@ -1,6 +1,34 @@
 import sharp from "sharp";
 import TextToSVG from "text-to-svg";
 import SVGToJpeg from "convert-svg-to-jpeg";
+import AWS from "aws-sdk";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import fs from "fs";
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-1'
+});
+
+const s3 = new S3Client({ region: "us-east-1" });
+
+async function uploadToS3(bucketName, filename, body) {
+  const params = {
+    Bucket: bucketName,
+    Key: filename,
+    Body: body,
+  };
+
+  const command = new PutObjectCommand(params);
+
+  try {
+    const data = await s3.send(command);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
 
 async function addTextToImage(
   imagePath,
@@ -42,33 +70,20 @@ async function addTextToImage(
       .toFile(outputPath);
 
     console.log("Image saved with text:", outputPath);
-    // Read the output file into a buffer
-
     return "File uploaded successfully";
   } catch (error) {
     return error.toString();
   }
 }
 
-export const handler = async (event) => {
-  const imagePath = "banner.jpeg";
-  const outputPath = "output-image.jpg";
-  const fontPath = "Arial.ttf";
-  const text = "5,341 subscribers to go!";
+const imagePath = "banner.jpeg";
+const fontPath = "Arial.ttf";
 
-  const output_msg = await addTextToImage(
-    imagePath,
-    text,
-    outputPath,
-    fontPath,
-    40
-  );
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ message: output_msg }),
-  };
-  return response;
-};
-
-handler(null)
+for (let i = 1; i <= 10000; i++) {
+  const text = `${i} subscribers to go!`;
+  const outputPath = `banner_${i}.jpg`;
+  await addTextToImage(imagePath, text, outputPath, fontPath, 40);
+  const data = await uploadToS3("lukej-banners", outputPath, fs.createReadStream(outputPath));
+  console.log(`File uploaded successfully: ${data.Location}`);
+}
